38e40bef6a5691dc7c833430bd91168dc0ff1dd9
KubaGithub92 <jakub.rosol@email.cz>
2022-10-15 21:13:49 +0200

    created story page and jsx for Form component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Form.scss'
+
+export default function Form() {
+  return (
+   <>
+   <form  className='form' action="save.php" method='post'>
+      <div className='form__inputs'>
+        <div className='form__inputs-item'>
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" name='first-name' id='first-name' required/>
+        </div>
+        <div className='form__inputs-item'>
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" name='last-name' id='last-name' required/>
+        </div>
+        <div className='form__inputs-item'>
+          <label htmlFor="phone-num">Phone Number</label>
+          <input type="tel" name='phone-num' id='phone-num' required/>
+        </div>
+        <div className='form__inputs-item'>
+          <label htmlFor="email">Email</label>
+          <input type="email" name='email' id='email' required/>
+        </div>
+      </div>
+
+      <div className='form__message'>
+        <label className="form__message-label" htmlFor="message">Your message</label>
+        <textarea className='form__message-textarea' name="message" id="message" cols="30" rows="10"></textarea>
+      </div>
+
+      <button className='form__button' type='submit'>Send message</button>
+   </form>
+   </>
+  )
+}
--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import Footer from '../components/Footer/Footer'
+import "../styles/Story.scss"
+import Form from '../components/Form/Form';
 
 function Story() {
   return (
-    <>
-      <h1>this is the story</h1>
-      <Footer/>
-    </>
+
+        <>
+          <section className='story'>
+            <div className='story__wrapper'>
+              <div className='story__content'>
+                  <h2 className='story__content-heading'>Our Story</h2>
+                  <p className='story__content-text'>GHC started as a project of two friends and passionate baristas, John & Don in 2010. 
+                     Believing as much in the hand of an educated barista as in the innovative equipment of the our ever-growing industry, GHC offers a full spectrum of brewing opportunities - from pour overs to syphon pots to finely executed espresso beverages. Each cup of coffee is made with love, precision and care.</p>
+              </div>
+      
+              <div className='story__images'>
+                <div className='story__image-wrapper'><img src={require("../img/coffee-cups.jpg")} alt="3 cups of coffee" /></div>
+                <div className='story__image-wrapper'><img src={require("../img/coffee-bag.jpg")} alt="bag full of coffee seeds" /></div>
+                <div className='story__image-wrapper'><img src={require("../img/coffee-seeds.jpg")} alt="coffee seeds" /></div>
+                <div className='story__image-wrapper'><img src={require("../img/pouring-coffee.jpg")} alt="pouring coffee" /></div>
+              </div>
+            </div>
+    
+          <Form/>
+    
+          </section>
+          <Footer/>
+        </>
   );
 }
 
